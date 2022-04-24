e33a8fbfe58cd9b0ede218029ba2e5265f0923ff
Harold Diaz <harold.diazv1@gmail.com>
2022-04-24 18:01:09 -0500

    Define function using arrow function

---
+++ arrowFunctions.ts
@@ -0,0 +1,11 @@
+// JS declarate function(expression way)
+let log = function(message){
+    console.log(message);
+}
+
+// TS declarate function(arrow function)
+let doLog = (message) => {
+    console.log(message)
+}
+//Exclude braces if have only 1 line
+let doLog2 = (message) => console.log(message);
